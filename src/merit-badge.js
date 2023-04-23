9924801048b9e48685a8f09ebf5e6ee2a4fae113
JeffTaylor108 <jeffta1080@gmail.com>
2023-04-23 19:19:13 -0400

    commit

--- src/merit-badge.js
+++ src/merit-badge.js
@@ -4,21 +4,32 @@ class MeritBadge extends LitElement {
   static properties = {
     date: { type: String },
     icon: { type: String },
-    title: { type: String }
+    title: { type: String },
+    unlocked: { type: Boolean}
   }
 
   static styles = css`
+
     .badge {
+      --black: #000000;
+      --dark-blue: #2E5984;
+      --light-blue: #ADD8E6;
+      --white: #FFFFFF;
+      --small: 0.7;
+      --medium: 1;
+      --large: 1.3;
       display: inline-table;
       vertical-align: middle;
       width: 220px;
       height: 220px;
+      margin: 10px;
       background-color: #bbb;
       border-style: dotted;
-      border-color: black;
+      border-color: var(--black);
       border-radius: 50%;
       padding: 10px;
       box-shadow: 0 0 0 2px #bbb, 3px 3px 6px 2px rgba(10, 10, 0, 0.5);
+      transform: scale(var(--medium));
     }
 
     .badge_content {
@@ -28,7 +39,7 @@ class MeritBadge extends LitElement {
     }
 
     h3 {
-      font-size: 20px;
+      font-size: 25px;
     }
 
     .logo img {
@@ -64,9 +75,10 @@ class MeritBadge extends LitElement {
 
   constructor() {
     super();
-    this.date = 'Sample Date';
+    this.date = 'LOCKED';
     this.icon = 'https://cdn-icons-png.flaticon.com/512/5405/5405811.png';
-    this.title = 'Sample Title';
+    this.title = 'Research Analysis';
+    this.unlocked = false;
   }
 
   render() {
